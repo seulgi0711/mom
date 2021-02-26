876ee029bb8258143444f6cb9bbdcac703bbcbe5
theo(성슬기) <theo.gt@kakaoenterprise.com>
2021-02-26 10:16:21 +0900

    add storage

---
+++ src/effects/storage.ts
@@ -0,0 +1,13 @@
+import { IO } from 'fp-ts/lib/IO';
+import { fromNullable, Option } from 'fp-ts/lib/Option';
+import { curry } from 'ramda';
+
+export const getItem = (key: string): IO<Option<string>> => () => {
+  return fromNullable(localStorage.getItem(key));
+};
+
+export const setItem = curry(
+  (key: string, value: string): IO<void> => () => {
+    return localStorage.setItem(key, value);
+  },
+);
